ede24876a442afe7a38d7aa1b8859e1361edaa19
slmanmktbabw-hue <slmanmktbabw@gmail.com>
2026-07-27 22:58:53 +0300

    Improve formatting and structure of sw.js
    Refactor service worker code for better readability and structure.

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,34 @@
 const CACHE_NAME = 'tarim-os-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
 });
