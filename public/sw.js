fe239781029a75bbb66735da00a1da02963b6bc4
slmanmktbabw-hue <slmanmktbabw@gmail.com>
2026-07-28 01:13:26 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,37 +1,5 @@
-const CACHE_NAME = 'tarim-os-v15-global';
-const assetsToCache = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(assetsToCache))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
-});
+const CACHE_NAME='tarim-os-v17-ceo';
+const assetsToCache=['/','/index.html','/script.js','/manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(assetsToCache)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k)}))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
