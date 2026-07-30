6a0568a1c00c9cb4fd5ca2aed642240d1745a1b0
slmanmktbabw-hue <slmanmktbabw@gmail.com>
2026-07-30 18:09:43 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,39 +1,15 @@
-const CACHE_NAME = 'tarim-os-v25-live';
-const assets = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('tarim-os-v20').then((cache) => {
+      return cache.addAll(['/', '/index.html', '/manifest.json']);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clientsClaim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
