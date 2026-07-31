543421980078ccaf54b78b1c77dd9bfecb57468e
slmanmktbabw-hue <slmanmktbabw@gmail.com>
2026-07-31 16:20:42 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,19 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('tarim-os-v20').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/manifest.json']);
-    })
-  );
+    e.waitUntil(
+        caches.open('tarim-os-v2.1').then((cache) => {
+            return cache.addAll(['/', '/index.html', '/script.js', '/manifest.json']);
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        }).catch(() => {
+            if(e.request.mode === 'navigate') {
+                return caches.match('/index.html');
+            }
+        })
+    );
 });
