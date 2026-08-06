cd1c588d5b9a63d25a94a0b3da1e7ef7da62cb1e
slmanmktbabw-hue <slmanmktbabw@gmail.com>
2026-08-06 19:23:45 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -246,5 +246,4 @@ function changeUserBackground() {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
     showToast('🎨 تم تغيير خلفية النظام الإمبراطوري بنجاح');
-                                          }
-    
+        }
