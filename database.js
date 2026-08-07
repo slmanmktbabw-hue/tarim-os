a8f53b644889d601d8112e5ce892941953c5c764
slmanmktbabw-hue <slmanmktbabw@gmail.com>
2026-08-07 16:31:25 +0300

    Refactor database.js to use static db object
    Replaced mock database functions with a static database object.

--- database.js
+++ database.js
@@ -1,18 +1,9 @@
-// database.js - Mock Database for TARIM OS
-const fs = require('fs');
-const path = require('path');
+// قاعدة بيانات محاكاة (Mock Database) لـ TARIM OS
+const db = {
+    users: [
+        { id: 1, username: "AL", role: "Emperor", okxBalance: 1000 }
+    ],
+    posts: []
+};
 
-const dbPath = path.join(__dirname, 'data.json');
-
-function getData() {
-    if (!fs.existsSync(dbPath)) {
-        fs.writeFileSync(dbPath, JSON.stringify({ users: [], posts: [] }, null, 2));
-    }
-    return JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-}
-
-function saveData(data) {
-    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
-}
-
-module.exports = { getData, saveData };
+module.exports = db;
